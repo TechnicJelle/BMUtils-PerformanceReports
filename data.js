aa4023b88b33332b9cb72dfcb50b01a3a8d1c1c2
github-action-benchmark <github@users.noreply.github.com>
2024-03-23 07:30:41 +0000

    add JMH Benchmark (jmh) benchmark result for f5ed778f43e02a8bb8dc0a1a3dda015fc9ec869d

---
+++ data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711179041179,
+  "repoUrl": "https://github.com/TechnicJelle/BMUtils",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "committer": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "distinct": true,
+          "id": "f5ed778f43e02a8bb8dc0a1a3dda015fc9ec869d",
+          "message": "Fully enable the performance reports! (2)",
+          "timestamp": "2024-03-23T04:42:12+01:00",
+          "tree_id": "84fcefb25574d2915f1285b3586ad0fe1ae7759c",
+          "url": "https://github.com/TechnicJelle/BMUtils/commit/f5ed778f43e02a8bb8dc0a1a3dda015fc9ec869d"
+        },
+        "date": 1711179040702,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese10",
+            "value": 84.10094685952636,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese11",
+            "value": 370.0557349756614,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese12",
+            "value": 1690.1330815333336,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese13",
+            "value": 7959.814793579999,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese14",
+            "value": 37486.22861523998,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese15",
+            "value": 160452.84572627998,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
