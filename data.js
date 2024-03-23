809112be53b746d67aa2e88c5e351b5e76e4fe47
github-action-benchmark <github@users.noreply.github.com>
2024-03-23 02:43:35 +0000

    add JMH Benchmark (jmh) benchmark result for 5b92ac24c772fe69db48777fde88a570949f9006

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1711161815344,
+  "repoUrl": "https://github.com/TechnicJelle/BMUtils",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "committer": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "distinct": true,
+          "id": "5b92ac24c772fe69db48777fde88a570949f9006",
+          "message": "Different path?",
+          "timestamp": "2024-03-23T03:42:42+01:00",
+          "tree_id": "a67561716b332e01f84d4fa526b7246317d0f0c6",
+          "url": "https://github.com/TechnicJelle/BMUtils/commit/5b92ac24c772fe69db48777fde88a570949f9006"
+        },
+        "date": 1711161814918,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese10",
+            "value": 82.50572267213114,
+            "unit": "ms/op",
+            "extra": "iterations: 1\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
