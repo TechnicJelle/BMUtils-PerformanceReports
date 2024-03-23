b60529ec6147eea841455b7511211f4ea4aa49bd
github-action-benchmark <github@users.noreply.github.com>
2024-03-23 18:12:52 +0000

    add JMH Benchmark (jmh) benchmark result for 92ae55867054a1181bfe5c91b626114e9bed1caa

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1711179041179,
+  "lastUpdate": 1711217572823,
   "repoUrl": "https://github.com/TechnicJelle/BMUtils",
   "entries": {
     "JMH Benchmark": [
@@ -58,6 +58,66 @@ window.BENCHMARK_DATA = {
           {
             "name": "com.technicjelle.CheeseBench.measureCheese15",
             "value": 160452.84572627998,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "committer": {
+            "email": "22576047+TechnicJelle@users.noreply.github.com",
+            "name": "TechnicJelle",
+            "username": "TechnicJelle"
+          },
+          "distinct": true,
+          "id": "92ae55867054a1181bfe5c91b626114e9bed1caa",
+          "message": "Last commit caused the overlaps to not be detected\n\nWhich is something I actually didn't want anyway, so this is a good improvement.\nI'll count this as a bugfix, so it's not a major API change.",
+          "timestamp": "2024-03-23T17:23:27+01:00",
+          "tree_id": "33f0e482face2cbeec1e69c388f922537f15cb2e",
+          "url": "https://github.com/TechnicJelle/BMUtils/commit/92ae55867054a1181bfe5c91b626114e9bed1caa"
+        },
+        "date": 1711217572562,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese10",
+            "value": 14.177082566258255,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese11",
+            "value": 55.201298164145044,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese12",
+            "value": 216.88385704407045,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese13",
+            "value": 2170.684161512,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese14",
+            "value": 8774.1300047,
+            "unit": "ms/op",
+            "extra": "iterations: 5\nforks: 5\nthreads: 1"
+          },
+          {
+            "name": "com.technicjelle.CheeseBench.measureCheese15",
+            "value": 40996.24612411999,
             "unit": "ms/op",
             "extra": "iterations: 5\nforks: 5\nthreads: 1"
           }
